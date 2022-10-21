dc171a05c5e924e4bbf845ea4afc29350544e83d
NickZh <62114203+NickZhYT@users.noreply.github.com>
2022-10-21 21:32:32 +0300

    Update app.py
    Basic Application Example

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+#Basic Application Example
+
 import server
 
 dp = server.Dispatcher()
 res = server.Resolver(dp)
 
-def example(_, head):
+def example(payload, headers): #payload - list, headers - list
     f = open("index.html", "r")
     file = f.read()
-    return file+"\n\nData: "+head[0]
+    return file+"\n\nPayload: "+str(payload)+"\n\nHeaders: "+str(headers)
 dp.add_handler(example, {"type": "GET", "path": "/"})
 
 res.polling("localhost", 80)
